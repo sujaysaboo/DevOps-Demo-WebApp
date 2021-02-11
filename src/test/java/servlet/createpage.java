90e74db8a4f32d6e969744197fef929a102a4d35
sujaysaboo <78503556+sujaysaboo@users.noreply.github.com>
2021-02-11 17:58:24 -0500

    Update createpage.java
    ip address change

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.220.46.42:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.223.120.255:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
